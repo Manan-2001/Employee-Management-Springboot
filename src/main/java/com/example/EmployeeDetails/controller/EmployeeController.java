8aa87741d6036e2b069cec02dd9fcd66138504d3
Manan-tutorology <mananjain.tutorology@gmail.com>
2024-09-15 13:15:38 +0530

    This is Spring boot project for Employee Management

--- src/main/java/com/example/EmployeeDetails/controller/EmployeeController.java
+++ src/main/java/com/example/EmployeeDetails/controller/EmployeeController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 
+@CrossOrigin(origins = "*")
 
 @AllArgsConstructor
 @RestController
@@ -19,7 +20,6 @@ import java.util.List;
 public class EmployeeController {
     private EmployeeService employeeService;
 
-    @CrossOrigin(origins = "http://localhost:4200")
     @PostMapping
     public ResponseEntity<EmployeeDTO> createEmployee(@RequestBody EmployeeDTO employeeDTO){
        EmployeeDTO savedEmployee= employeeService.createEmployee(employeeDTO);
